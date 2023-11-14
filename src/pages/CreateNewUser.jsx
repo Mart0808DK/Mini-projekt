43d0f06b558bb464b4efe5ff611ddef46f5deb3c
emilvn <emilvnielsen@hotmail.com>
2023-11-14 11:12:36 +0100

    done

--- src/pages/CreateNewUser.jsx
+++ src/pages/CreateNewUser.jsx
@@ -13,11 +13,11 @@ function CreateNewUser({create}) {
     return (
         <div className="flex justify-center items-center h-screen">
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-80 bg-slate-800 p-4 rounded">
-                <h2 className="text-2xl text-center">Add user</h2>
-                <label htmlFor="name-input">name</label>
-                <input className="text-slate-950" id="name-input" type="text" placeholder="name" name="name"/>
-                <label htmlFor="age-input">age</label>
-                <input className="text-slate-950" id="age-input" type="number" placeholder="age" name="age"/>
+                <h2 className="text-2xl text-center font-bold">Add user</h2>
+                <label htmlFor="name-input">Name</label>
+                <input className="text-slate-950 rounded p-2 outline-none" id="name-input" type="text" placeholder="Write user name..." name="name"/>
+                <label htmlFor="age-input">Age</label>
+                <input className="text-slate-950 rounded p-2 outline-none" id="age-input" type="number" placeholder="Write user age..." name="age"/>
                 <div className="flex justify-center items-center mt-2">
                     <button type="submit" className="bg-slate-950 w-20 p-2 rounded hover:bg-slate-900 transition-colors">Submit</button>
                 </div>
